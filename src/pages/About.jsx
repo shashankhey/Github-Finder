4ec4fe5ed30dadc8a581fd22bf84930e25c72e5f
shashankhey <100605358+shashankhey@users.noreply.github.com>
2023-01-09 09:37:06 +0530

    Completed

---
+++ src/pages/About.jsx
@@ -0,0 +1,27 @@
+
+
+function About() {
+  return (
+    <>
+    <h1 className='text-6xl mb-4'>Github Finder</h1>
+    <p className='mb-4 text-2xl font-light'>
+      A React app to search GitHub profiles and see profile details. By:-
+      <strong>
+        <a href='https://www.linkedin.com/in/shashank-1a8049131/'> Shashank</a>
+      </strong>
+      .
+    </p>
+    <p className='text-lg text-gray-400'>
+      Version <span className='text-white'>1.0.0</span>
+    </p>
+    <p className='text-lg text-gray-400'>
+      Layout By:
+      <a className='text-white' href='https://www.linkedin.com/in/shashank-1a8049131/'>
+        Hassib Moddasser
+      </a>
+    </p>
+  </>
+  )
+}
+
+export default About
